af877bde61f054045c1db4e38c24ef2eb66be316
Titus Muthomi <muthomititus+github@gmail.com>
2023-06-03 10:58:15 +0300

    Add books to list and manipulate them

--- BookStore.java
+++ BookStore.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class BookStore {
     
-  
+  private List<Book> books = new ArrayList<>();
+
+
+  public void addBook(Book book) {
+    books.add(book);
+  }
+
+  public void updateBook(Book book) {
+    books.set(books.indexOf(book), book);
+  }
+
+  public void removeBook(Book book) {
+    books.remove(book);
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+
+
 }
